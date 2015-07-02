bb253b8a455536d2780f6393f602f12098492cad
549642238 <549642238@qq.com>
2015-07-02 13:52:47 +0800

    Project	7.2/2015

--- Project/JCreator/My_qq.1ed/Communication.java
+++
@@ -1,8 +0,0 @@
-package MYQQ.communication;
-
-public class Communication{
-	public static String s=new String("");
-	public static int count=0;
-	public static int currentCount=0;
-	public static int lock=0;
-}
